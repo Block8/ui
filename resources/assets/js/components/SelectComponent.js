a09c77d4ed2ddaee33016108151f354447beb3ff
Dan Cryer <dan@block8.co.uk>
2018-04-24 11:36:31 +0100

    Force select2 to use placeholder. Allow clear for non-required select boxes.

--- resources/assets/js/components/SelectComponent.js
+++ resources/assets/js/components/SelectComponent.js
@@ -2,6 +2,13 @@
 class SelectComponent extends AdminUiComponent
 {
     ready() {
-        $('select:not(.no-replace)').select2();
+        $('select:not(.no-replace)').each(function () {
+            let $sel = $(this);
+
+            $sel.select2({
+                allowClear: !$sel.prop('required'),
+                placeholder: 'Please select...',
+            });
+        });
     }
 }
--- resources/views/forms/select.blade.php
+++ resources/views/forms/select.blade.php
@@ -2,6 +2,8 @@
     <label for="{{ $id or $name }}" class="control-label">{{ $label }}</label>
 
     <select id="{{ $id or $name }}" class="form-control {{ $class or '' }}" name="{{ $name }}" {{ isset($required) && $required ? 'required' : '' }}>
+        <option></option>
+
         @if(isset($options))
             @foreach($options as $key => $label)
                 <option value="{{ $key }}" {{ old($name, $value ?? null) == $key ? 'selected' : ''}}>{{ $label }}</option>
