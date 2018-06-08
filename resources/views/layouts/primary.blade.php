2eaa001b909045ed68b0818867cc4004b141888f
Dan Cryer <dan@block8.co.uk>
2018-06-08 12:24:49 +0100

    Update primary.blade.php

--- resources/views/layouts/primary.blade.php
+++ resources/views/layouts/primary.blade.php
@@ -70,7 +70,11 @@
 </div>
 
 <!-- Scripts -->
-<script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+@if(file_exists(public_path('fa/js/fontawesome-all.js')))
+    <script defer src="/fa/js/fontawesome-all.js"></script>
+@else
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
+@endif
 
 <script src="/block8/ui/js/jquery.min.js"></script>
 <script src="/block8/ui/js/moment.min.js"></script>
