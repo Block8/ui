311f78791c94c9366c0ae9ccd83ac3e02827fda1
Dan Cryer <dan@block8.co.uk>
2018-06-14 10:54:11 +0100

    Updating dependencies.

--- resources/views/forms/image-upload.blade.php
+++ resources/views/forms/image-upload.blade.php
@@ -1,5 +1,7 @@
-@php($name = $name ?? $key)
-@php($value = $value ?? $image)
+@php
+$name = $name ?? $key;
+$value = $value ?? $image;
+@endphp
 <div class="form-group image-upload {{ $errors->has($name) ? 'has-error' : '' }}">
     <label for="{{ $name }}" class="control-label">{{ $label }}</label>
     <input id="{{ $name }}" name="{{ $name }}" type="file" {{ isset($required) && $required ? 'required' : '' }}>
@@ -10,7 +12,7 @@
         </span>
     @endif
 
-    @isset($value)
+    @if(!empty($value))
         <img src="{{ $value }}" style="display: block; max-width: 300px; max-height: 300px; margin: 15px 0; padding: 5px; box-shadow: 0 0 5px rgba(0,0,0,0.5);">
-    @endisset
+    @endif
 </div>
