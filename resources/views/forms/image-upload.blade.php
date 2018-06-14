69517f2a4e815bf67521bfeb0da7aa444f4b2173
Dan Cryer <dan@block8.co.uk>
2018-06-14 10:15:46 +0100

    Fix the image upload component

--- resources/views/forms/image-upload.blade.php
+++ resources/views/forms/image-upload.blade.php
@@ -1,6 +1,8 @@
+@php($name = $name ?? $key)
+@php($value = $value ?? $image)
 <div class="form-group image-upload {{ $errors->has($name) ? 'has-error' : '' }}">
-    <label for="{{ $key }}" class="control-label">{{ $label }}</label>
-    <input id="{{ $key }}" name="{{ $key }}" type="file" {{ isset($required) && $required ? 'required' : '' }}>
+    <label for="{{ $name }}" class="control-label">{{ $label }}</label>
+    <input id="{{ $name }}" name="{{ $name }}" type="file" {{ isset($required) && $required ? 'required' : '' }}>
 
     @if ($errors->has($name))
         <span class="help-block">
@@ -8,7 +10,7 @@
         </span>
     @endif
 
-    @isset($image)
-        <img src="{{ $image }}" style="display: block; max-width: 300px; max-height: 300px; margin: 15px 0; padding: 5px; box-shadow: 0 0 5px rgba(0,0,0,0.5);">
+    @isset($value)
+        <img src="{{ $value }}" style="display: block; max-width: 300px; max-height: 300px; margin: 15px 0; padding: 5px; box-shadow: 0 0 5px rgba(0,0,0,0.5);">
     @endisset
 </div>
